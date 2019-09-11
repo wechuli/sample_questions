7ce69180c10da78a18432b73a98da337bf0aec58
Paul Wechuli <wechuli@Pauls-MacBook-Pro.local>
2019-09-11 19:45:00 +0300

    question 1 - SQL

---
+++ question1/sol.sql
@@ -0,0 +1,32 @@
+-- SQL Dialect - PostgreSQL
+
+
+-- Make the table
+CREATE TABLE CarSales
+(
+    CarMaker           varchar(255) NOT NULL,
+    CarModel           varchar(255) NOT NULL,
+    SalePriceInDollars float,
+    SaleDate           date
+);
+-- Insert some Sample Data
+INSERT INTO CarSales(CarMaker, CarModel, SalePriceInDollars, SaleDate)
+VALUES ('Maker A', 'A Model X V6', 20000, '2019-09-03'),
+       ('Maker B', 'B Model V V6', 18500, '2019-08-03'),
+       ('Maker C', 'C Model X V6', 21500, '2019-09-01'),
+       ('Maker A', 'A Model X V6', 12000, '2019-08-31'),
+       ('Maker B', 'B Model X V6', 16783, '2019-08-28'),
+       ('Maker C', 'C Model X V6', 30000, '2019-07-23'),
+       ('Maker A', 'A Model V V6', 21000, '2019-08-23'),
+       ('Maker B', 'B Model X V6', 11000, '2019-09-03'),
+       ('Maker C', 'C Model X V6', 14300, '2019-09-02'),
+       ('Maker A', 'A Model V V6', 22000, '2019-09-03');
+
+
+-- Write a SQL query to get the total sale price per maker and model from the past 30 days
+
+SELECT CarMaker,CarModel,sum(SalePriceInDollars) TotalSalePrice
+FROM CarSales
+WHERE SaleDate > now() - INTERVAL '30 days'
+GROUP BY
+ROLLUP (CarMaker,CarModel);
